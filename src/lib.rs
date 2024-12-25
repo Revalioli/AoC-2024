fcae77ad85b5d33053ca11f3cd7e3fefdcb67c80
Revalioli <elinor.montmasson@gmail.com>
2024-12-26 00:48:34 +0100

    lib: add functions and trait to create structs from regex result
    Create the FromRegex trait and implement it for Vec of tuple of size
    1 to 7. Each tuple is filled with the capture groups of each regex
    match, and string parsing to other types is automated.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 /// Advent of Code utility functions.
-/// 
+///
 /// This crate provides utility functions to parse Advent of Code inputs.
 
-use std::str::FromStr;
+use std::{fmt::Debug, str::FromStr};
+use regex::Regex;
 
 /// Split a string following the given delimiters, parse each elements as T and return a
 /// Vec of the elements.
-/// 
+///
 /// `delimiters` is handle as in [`str::split()`].
 pub fn split_parse<T: FromStr>(line: &str, delimiters: &[char]) -> Result<Vec<T>, T::Err>
 {
     let mut res = Vec::new();
-    
+
     for i in line.split(delimiters) {
         res.push(i.parse()?);
     }
@@ -20,7 +21,7 @@ pub fn split_parse<T: FromStr>(line: &str, delimiters: &[char]) -> Result<Vec<T>
 }
 
 /// Split and parse each line of an input string.
-/// 
+///
 /// See [`split_parse()`]
 pub fn split_parse_lines<T: FromStr>(input: &str, delimiters: &[char])
     -> Result<Vec<Vec<T>>, T::Err>
@@ -32,8 +33,87 @@ pub fn split_parse_lines<T: FromStr>(input: &str, delimiters: &[char])
     Ok(res)
 }
 
+/// Create a struct instance from the matching result of a regex to a string.
+pub trait FromRegex : Sized {
+
+    type Err;
+
+    /// Create a new instance from the match result of a regex on a string.
+    ///
+    /// This is essentially used with generic Vec of tuples to easily convert
+    /// multiple matches and there capture groups to usable tuple instances.
+    fn from_regex(r: &Regex, s: &str) -> Result<Self, Self::Err>;
+
+}
+
+/// Type error when parsing a regex capture result to a tuple.
+#[derive(Debug)]
+pub enum TupleParseRegexError {
+    WrongCaptureGroups{ expected: usize, nb_groups: usize },
+    TypeParsing(String),
+}
+
+
+macro_rules! vec_tuple_impl_from_regex {
+    ( $($T:ident),+ , $count:literal ) => {
+
+        impl< $($T),+ > FromRegex for Vec<( $($T,)+ )>
+        where
+            $( $T: FromStr<Err: Debug> ),+
+        {
+            type Err = TupleParseRegexError;
+
+            fn from_regex(r: &Regex, s: &str) -> Result<Self, Self::Err> {
+                let mut ret = Vec::new();
+
+                for c in r.captures_iter(s) {
+                    if c.len()-1 != $count {
+                        return Err(TupleParseRegexError::WrongCaptureGroups{expected: $count, nb_groups: c.len()-1});
+                    }
+
+                    let mut captures = c.extract::<$count>().1.into_iter();
+                    let to_push = (
+                        $(
+                            captures.next().unwrap()
+                            .parse::<$T>().map_err(|err: $T::Err|
+                                TupleParseRegexError::TypeParsing(format!("{err:?}"))
+                            )?,
+                        )+
+                    );
+
+                    ret.push( to_push );
+                }
+
+                Ok(ret)
+            }
+        }
+
+    };
+}
+
+vec_tuple_impl_from_regex!{T, 1}
+vec_tuple_impl_from_regex!{T, U, 2}
+vec_tuple_impl_from_regex!{T, U, V, 3}
+vec_tuple_impl_from_regex!{T, U, V, W, 4}
+vec_tuple_impl_from_regex!{T, U, V, W, X, 5}
+vec_tuple_impl_from_regex!{T, U, V, W, X, Y, 6}
+vec_tuple_impl_from_regex!{T, U, V, W, X, Y, Z, 7}
+
+
+/// Create an instance of `T` for each line based on the result of a regex over a string.
+pub fn regex_parse_lines<T: FromRegex>(input: &str, regex: &Regex) -> Result<Vec<T>, T::Err> {
+    let mut ret = Vec::new();
+
+    for l in input.lines() {
+        ret.push(T::from_regex(&regex, l)?);
+    }
+
+    Ok(ret)
+}
+
 
 #[cfg(test)]
+#[allow(non_snake_case)]
 mod tests {
     use super::*;
 
@@ -69,5 +149,55 @@ mod tests {
 
         assert!(split_parse_lines::<f64>(wrong_input, &[':', '=']).is_err());
 
+    }
+
+
+    #[test]
+    fn aoc_lib_tuple_2_impl_FromRegex_test() {
+        let s = r"5to95.9  mkg jz ¨878fdsljz95.55879 jnlkjhe-778.96to7895to45.2";
+        let r = Regex::new( r"(\d+)to(\d+\.\d+)").unwrap();
+
+        let res= Vec::<(u64, f32)>::from_regex(&r, s);
+        assert!(res.is_ok(), "{res:?}");
+        let res = res.unwrap();
+        println!("{res:?}");
+        assert_eq!( res, [ (5, 95.9), (7895, 45.2) ] );
+
+        // No match
+        let res = Vec::<(u64, f32)>::from_regex(&r, "foobar");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap().len(), 0);
+    }
+
+
+    #[test]
+    fn aoc_lib_tuple_3_impl_FromRegex_test() {
+        let s = r"5to95.9  mkg jz ¨878fdsljz95.55879 jnlkjhe-778.96to7895to45.2,iepgl1556to12.45456foo";
+        let r = Regex::new( r"(\d+)to(\d+\.\d+)(.{3})").unwrap();
+
+        let res= Vec::<(u64, f32, String)>::from_regex(&r, s);
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        println!("{res:?}");
+        assert_eq!( res, [
+            (5, 95.9, String::from("  m")),
+            (7895, 45.2, String::from(",ie")),
+            (1556, 12.45456, String::from("foo"))
+        ]);
+    }
+
+    #[test]
+    fn aoc_lib_tuple_impl_FromRegex_error_test() {
+        let s = r"foo 7889 bar";
+        let r_wrong_type = Regex::new(r"(foo)").unwrap();
+        let r_wrong_nb_groups = Regex::new(r"(\d*) (ba)r").unwrap();
+
+        let res = Vec::<(u32,)>::from_regex(&r_wrong_type, s);
+        assert!(res.is_err());
+        assert!(matches!(res.unwrap_err(), TupleParseRegexError::TypeParsing(_)));
+
+        let res = Vec::<(u32,)>::from_regex(&r_wrong_nb_groups, s);
+        assert!(res.is_err());
+        assert!(matches!(res.unwrap_err(), TupleParseRegexError::WrongCaptureGroups{expected: 1, nb_groups: 2}));
     }
 }
